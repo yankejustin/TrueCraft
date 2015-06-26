4bfe57c9469980d566e9f56ceed06b7768a92108
William Moorehouse <moorehousew@gmail.com>
2015-06-25 20:28:20 -0400

    Added support for OpenGL exceptions

---
+++ TrueCraft.Client.Runtime/Graphics/OpenGL/ErrorCode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TrueCraft.Client.Graphics.OpenGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>Enumeration values taken from the OpenTK project. [https://github.com/opentk/opentk/]</remarks>
+    public enum ErrorCode
+        : int
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        NoError = 0,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        InvalidEnum = 0x0500,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        InvalidValue = 0x0501,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        InvalidOperation = 0x0502,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        StackOverflow = 0x0503,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        StackUnderflow = 0x0504,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        OutOfMemory = 0x0505,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        InvalidFramebufferOperation = 0x0506,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        ContextLost = 0x0507
+    }
+}
---
+++ TrueCraft.Client.Runtime/Graphics/OpenGL/OpenGLException.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace TrueCraft.Client.Graphics.OpenGL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class OpenGLException
+        : GraphicsException
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        internal static void CheckErrors()
+        {
+            var error = (ErrorCode)GL.GetError();
+            if (error != ErrorCode.NoError)
+                throw new OpenGLException(error);
+        }
+
+        private ErrorCode _errorCode;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ErrorCode ErrorCode
+        {
+            get { return _errorCode; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="error"></param>
+        public OpenGLException(ErrorCode errorCode)
+            : base()
+        {
+            _errorCode = errorCode;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="errorCode"></param>
+        public OpenGLException(string message, ErrorCode errorCode)
+            : base(message)
+        {
+            _errorCode = errorCode;
+        }
+    }
+}
